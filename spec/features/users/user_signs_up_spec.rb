b659b9f23114b9fbd6d37612363de93c81a9d766
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-01-18 16:56:37 -0500

    User Settings

--- spec/features/users/user_signs_up_spec.rb
+++ spec/features/users/user_signs_up_spec.rb
@@ -6,24 +6,27 @@ feature 'new user signs up', %{
   So I can post comments about bars
 
   Acceptance Criteria
-  [] I will create an account from sign up page
-  [] I must specify a valid username
-  [] I must specify a valid e-mail
-  [] I must specify a password and confirm that password
-  [] I get error messages if I do not perform the above
-  [] I register my account and am authenticated if I specify valid information
+  [X] I will create an account from sign up page
+  [X] I must specify a valid username
+  [X] I must specify a valid e-mail
+  [X] I must specify a password and confirm that password
+  [X] I get error messages if I do not perform the above
+  [X] I register my account and am authenticated if I specify valid information
 } do
 
   scenario "prospective user gets to sign up page from root path" do
     visit root_path
-    click_on "Sign Up"
+    click_link "Sign In"
+    save_and_open_page
+    click_link "Sign up"
     expect(page).to have_content("Sign Up")
     expect(page).to have_content("Password confirmation")
   end
 
   scenario "prospective user correctly submits sign up form" do
     visit root_path
-    click_link 'Sign Up'
+    click_link "Sign In"
+    click_link 'Sign up'
     fill_in 'Username', with: 'Pablo'
     fill_in 'Email', with: 'escobar457@gmail.com'
     fill_in 'Password', with: 'money$17'
@@ -35,7 +38,8 @@ feature 'new user signs up', %{
 
   scenario 'required information is not supplied' do
     visit root_path
-    click_link 'Sign Up'
+    click_link "Sign In"
+    click_link 'Sign up'
     click_button 'Sign Up'
     expect(page).to have_content("can't be blank")
     expect(page).to_not have_content('Sign Out')
@@ -43,7 +47,8 @@ feature 'new user signs up', %{
 
   scenario 'password does not match confirmation' do
     visit root_path
-    click_link 'Sign Up'
+    click_link "Sign In"
+    click_link 'Sign up'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'morepassword'
     click_button 'Sign Up'
