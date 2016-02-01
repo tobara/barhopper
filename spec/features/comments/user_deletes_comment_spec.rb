d2d5b20395600fb0ce20df1f5ad483967d912f63
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-02-01 11:06:15 -0500

    tests pass again

--- spec/features/comments/user_deletes_comment_spec.rb
+++ spec/features/comments/user_deletes_comment_spec.rb
@@ -1,45 +1,45 @@
-# require "rails_helper"
+require "rails_helper"
 
-# feature 'user deletes comment', %{
-#   As a user
-#   I want to delete a comment
-#   Acceptance Criteria
-#   [X] I can delete a comment
-# } do
+feature 'user deletes comment', %{
+  As a user
+  I want to delete a comment
+  Acceptance Criteria
+  [X] I can delete a comment
+} do
 
-#   let!(:user) { FactoryGirl.create(:user) }
-#   let!(:user2) { FactoryGirl.create(:user) }
-#   let!(:bar) { FactoryGirl.create(:bar, user: user) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:user2) { FactoryGirl.create(:user) }
+  let!(:bar) { FactoryGirl.create(:bar, user: user) }
 
-#   before(:each) do
-#     visit new_user_session_path
+  before(:each) do
+    visit new_user_session_path
 
-#     fill_in "Username", with: user.username
-#     fill_in "Password", with: user.password
-#     click_button "Sign In"
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Sign In"
 
-#     visit bar_path(bar)
+    visit bar_path(bar)
 
-#     click_link 'Post Comment'
-#     fill_in 'Description', with: 'Sweet.'
-#     fill_in 'Rating', with: '9'
-#     click_button 'Add Comment'
-#   end
+    click_link 'Post Comment'
+    fill_in 'Description', with: 'Sweet.'
+    fill_in 'Rating', with: '9'
+    click_button 'Add Comment'
+  end
 
-#   scenario "an authorized user can delete a comment" do
-#     click_button "Delete"
-#     expect(page).to have_content('Comment Destroyed!')
-#     expect(page).to_not have_content('Sweet.')
-#   end
+  scenario "an authorized user can delete a comment" do
+    click_button "Delete"
+    expect(page).to have_content('Comment Destroyed!')
+    expect(page).to_not have_content('Sweet.')
+  end
 
-#   scenario "only authorized user can delete comment" do
-#     click_link 'Sign Out'
-#     visit new_user_session_path
-#     fill_in "Username", with: user2.username
-#     fill_in "Password", with: user2.password
-#     click_button "Sign In"
+  scenario "only authorized user can delete comment" do
+    click_link 'Sign Out'
+    visit new_user_session_path
+    fill_in "Username", with: user2.username
+    fill_in "Password", with: user2.password
+    click_button "Sign In"
 
-#     visit bar_path(bar)
-#     expect(page).to_not have_content('Delete')
-#   end
-# end
+    visit bar_path(bar)
+    expect(page).to_not have_content('Delete')
+  end
+end
