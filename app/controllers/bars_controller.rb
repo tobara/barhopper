b5ce7838221f03c2847d328db2e198e6a8e5d6a1
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-02-01 10:38:12 -0500

    Skinny controller

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -17,37 +17,10 @@ class BarsController < ApplicationController
 
   def index
     @bars = Bar.page(params[:page])
-    n = 0
-    @bars.each do |bar|
-      @bar_name = bar.name
-      @bar_name += " #{bar.location}"
-      @bar_name.gsub(" ","+")
-      url = "http://www.google.com/search?q="+"#{@bar_name}"+"&num=10"
-      agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
-      html = agent.get(url).body
-      doc = Nokogiri::HTML(html)
-      doc_string = doc.to_s
-      doc_find = nil
-      if result = doc_string.match(/lubh-bar( _...)/)
-        doc_find = result.captures
-        div_end = doc_find[0]
-        div_find = "lubh-bar"+"#{div_end}"
-        doc_at = doc.xpath("//div[@class=\"#{div_find}\"]")
-      end
-      if doc_at.nil?
-        bar_pop = 0
-      else
-        doc_style = doc_at[0]["style"]
-        bar_pop = doc_style.gsub(/[^0-9]/, '')
-      end
-      bar.attributes = { :popular_time => bar_pop }
-      pop_value = bar.popular_time.to_i
-      n += 1
-      bar.attributes = { :bar_img => "bar#{n}" }
-    end
-      @time = Time.new
-      @day = @time.strftime('%A')+"\'s"
-      @hour = @time.strftime('%I-%p')
+    Bar.popular_times(@bars)
+    @time = Time.new
+    @day = @time.strftime('%A')+"\'s"
+    @hour = @time.strftime('%I-%p')
   end
 
   def show
