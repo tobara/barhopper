663f70b2636f9cb6f20c1c38af802a5083466239
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-02-17 13:40:05 -0500

    small refactoring changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,32 +13,38 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require jquery.barfiller.js
-//= jquery.barfiller.js
-//= jquery-1.11.2.min.js
-//= jquery.easing.1.3.js
-//= bootstrap.min.js
-//= SmoothScroll.js
-//= jquery.scrollTo.min.js
-//= jquery.localScroll.min.js
-//= jquery.viewport.mini.js
-//= jquery.countTo.js
-//= jquery.appear.js
-//= jquery.sticky.js
-//= jquery.parallax-1.1.3.js
-//= jquery.fitvids.js
-//= owl.carousel.min.js
-//= isotope.pkgd.min.js
-//= imagesloaded.pkgd.min.js
-//= jquery.magnific-popup.min.js
-//= http://maps.google.com/maps/api/js?sensor=false&amp;language=en
-//= gmap3.min.js
-//= wow.min.js
-//= masonry.pkgd.min.js
-//= jquery.simple-text-rotator.min.js
 //= all.js
+//= bootstrap.min.js
 //= contact-form.js
+//= gmap3.min.js
+//= imagesloaded.pkgd.min.js
+//= isotope.pkgd.min.js
+//= jquery-1.11.2.min.js
 //= jquery.ajaxchimp.min.js
+//= jquery.appear.js
+//= jquery.backgroundvideo.min.js
+//= jquery.barfiller.js
+//= juery.countTo.js
+//= jquery.easing.1.3.js
+//= jquery.fitvids.js
+//= jquery.localScroll.min.js
+//= jquery.magnific-popup.min.js
+//= jquery.mb.YTPlayer.js
+//= jquery.parallax-1.1.3.js
+//= jquery.scrollTo.min.js
+//= jquery.simple-text-rotator.min.js
+//= jquery.sticky.js
+//= jquery.viewport.mini.js
+//= masonry.pkgd.min.js
+//= owl.carousel.min.js
+//= placeholder.js
+//= rev-slider.js
+//= SmoothScroll.js
+//= tiltfx.js
+//= wow.min.js
+
+//= http://maps.google.com/maps/api/js?sensor=false&amp;language=en
+
 
 
 
