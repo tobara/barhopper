d714a0312749f96a5d3a0744c8d209c6274fd74d
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-12-12 13:49:06 -0500

    a little cleaner, but much to do

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,38 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap.min
 //= require_tree .
-//= all.js
-//= bootstrap.min.js
-//= contact-form.js
-//= gmap3.min.js
-//= imagesloaded.pkgd.min.js
-//= isotope.pkgd.min.js
-//= jquery-1.11.2.min.js
-//= jquery.ajaxchimp.min.js
-//= jquery.appear.js
-//= jquery.backgroundvideo.min.js
-//= jquery.barfiller.js
-//= juery.countTo.js
-//= jquery.easing.1.3.js
-//= jquery.fitvids.js
-//= jquery.localScroll.min.js
-//= jquery.magnific-popup.min.js
-//= jquery.mb.YTPlayer.js
-//= jquery.parallax-1.1.3.js
-//= jquery.scrollTo.min.js
-//= jquery.simple-text-rotator.min.js
-//= jquery.sticky.js
-//= jquery.viewport.mini.js
-//= masonry.pkgd.min.js
-//= owl.carousel.min.js
-//= placeholder.js
-//= rev-slider.js
-//= SmoothScroll.js
-//= tiltfx.js
-//= wow.min.js
 
-//= http://maps.google.com/maps/api/js?sensor=false&amp;language=en
 
 
 
